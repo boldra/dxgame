c5ed578e2443d7e4aea580462272eb816a20a988
Paul Boldra (D061876) <p.boldra@sap.com>
2015-01-22 14:57:36 +0100

    The deck now tracks used and unused cards

--- lib/DxGame/Deck.pm
+++ lib/DxGame/Deck.pm
@@ -2,18 +2,26 @@ package DxGame::Deck;
 use Moose;
 use List::Util qw<>;
 
-has cards => (
+#<<<
+has unused_cards => (
     is          => 'rw',
     isa         => 'ArrayRef',
     traits          => [qw<Array>],
 );
+
+has used_cards => (
+    is          => 'rw',
+    isa         => 'ArrayRef',
+    traits          => [qw<Array>],
+);
+#>>>
 
 
 sub BUILD {
     # shuffle
     my @cards = qw<1..100>;
     @cards = List::Util::shuffle(@cards);
-    $_[0]->cards(\@cards);
+    $_[0]->unused_cards(\@cards);
 }
 
 1;
